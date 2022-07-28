cf255e636564ef9a53d0303fb21f2e2c581c8af9
Artur <artur.alimgafarov@gmail.com>
2022-07-29 03:32:23 +0500

    Creating first agent

---
+++ Factory.bat
@@ -0,0 +1,2 @@
+C:/"Program Files"/Java/jdk-18/bin/java -cp "lib/jade.jar;lib/hamcrest-core-1.1.jar;lib/json-simple-1.1.1.jar;out/production/MultiAgentSystem" jade.Boot -container -container-name Factory -host "192.168.0.112" -port "1099"  -agents "MainAgent:MainAgent()"
+pause
