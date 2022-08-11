f25bfb941559fb9cf50fc176441734dcf2a4c203
Artur <artur.alimgafarov@gmail.com>
2022-08-11 18:07:33 +0500

    Version with output to JSON file (success finish program)

--- src/MainAgent.java
+++ src/MainAgent.java
@@ -9,7 +9,6 @@ import jade.wrapper.AgentController;
 import jade.wrapper.ControllerException;
 import jade.wrapper.StaleProxyException;
 
-import org.apache.poi.ss.usermodel.*;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -20,12 +19,6 @@ import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
-
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.hssf.usermodel.HSSFRow;
-import org.apache.poi.hssf.usermodel.HSSFCell;
-
 
 public class MainAgent extends Agent {
     public long _workersCount;
@@ -219,58 +212,46 @@ public class MainAgent extends Agent {
                         if (_workersCounter == _workersCount) {
                             System.out.println("\n*** All Workers reported. ***\n");
 
+                            // JSON data
+                            JSONObject jsonWorkers = new JSONObject();
+
                             // logging schedule
                             int i = 1;
-                            int maxWorkTime = 0;
                             for (var keyWorker: _scheduler.keySet()) {
                                 System.out.println((i++) + ". " + keyWorker + ":");
-                                int workTimeCounter = 0;
                                 var schedule = _scheduler.get(keyWorker);
+                                JSONObject jsonOrders = new JSONObject();
                                 for (var keyOrder : schedule.keySet()) {
-                                    int dur = schedule.get(keyOrder);
-                                    System.out.println("\t" + keyOrder + " = " + dur);
-                                    workTimeCounter += dur;
+                                    var duration = schedule.get(keyOrder);
+                                    System.out.println("\t" + keyOrder + " = " + duration);
+                                    jsonOrders.put(keyOrder, duration);
                                 }
-
-                                if (workTimeCounter > maxWorkTime) {
-                                    maxWorkTime = workTimeCounter;
-                                }
+                                jsonWorkers.put(keyWorker, jsonOrders);
                             }
 
                             // output schedule to file
-                            Workbook workbook = new HSSFWorkbook();
-                            Sheet sheet = workbook.createSheet("schedule");
+                            FileWriter file = null;
+                            try {
+                                file = new FileWriter("output.json");
+                                file.write(jsonWorkers.toJSONString());
 
-                            i = 1;
-                            for (var keyWorker: _scheduler.keySet()) {
-                                Row row = sheet.createRow(i++);
-                                Cell cell = row.createCell(0, CellType.STRING);
-                                cell.setCellValue(keyWorker);
-                                var schedule = _scheduler.get(keyWorker);
-                                for (var keyOrder: schedule.keySet()) {
-                                    int count = schedule.get(keyOrder);
-                                    for (int j = 0; j < count; j++) {
-                                        cell = row.createCell(j + 1, CellType.STRING);
-                                        cell.setCellValue(keyOrder);
-                                    }
+                            } catch (IOException e) {
+                                e.printStackTrace();
+
+                            } finally {
+
+                                try {
+                                    file.flush();
+                                    file.close();
+                                } catch (IOException e) {
+                                    // TODO Auto-generated catch block
+                                    e.printStackTrace();
                                 }
                             }
 
-                            File file = new File("C:/Users/ARTURIO/Desktop/output.xls");
-                            FileOutputStream outFile = null;
-
+                            // deleting Main agent
                             try {
-                                outFile = new FileOutputStream(file);
-                                workbook.write(outFile);
-                                outFile.close();
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-
-
-                            // deleting agents container
-                            try {
-                                getContainerController().kill();
+                                getContainerController().getAgent(getLocalName()).kill();
                             } catch (ControllerException e) {
                                 e.printStackTrace();
                             }
@@ -285,5 +266,6 @@ public class MainAgent extends Agent {
     protected void takeDown() {
         System.out.println("\n*** Program completed. ***\n");
         System.out.println(getAID().getLocalName() + " destroyed.");
+        System.exit(0);
     }
 }
